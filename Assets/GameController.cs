5ebcad3eff667a9f7611e36c31c325e2fbbc6f5c
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-03-10 18:24:28 -0600

    3/10

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using.UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
-public class new : MonoBehaviour
+public class GameController : MonoBehaviour
 {
    
 
@@ -20,49 +20,84 @@ public class new : MonoBehaviour
     public bool Done;
 
 
-void Newblock()
-{
-    if(lastCube != null)
+    void Newblock()
     {
-       // if (currentCube.transform.localScale.x <= 0f ||
-            //currentCube.transform.localScale.z <= 0f)
-       // {
-           // Done = true;
-            //text.GameObject.SetActive(true);
-//text.text = "Final Score " + Level;
-            //StartCoroutine(X());
-            //return;
-      //  }
-
-
-
-
-
-
-        currentCube.transform.position = new Vector3(Mathf.Round(currentCube.transform.position.x),
-            currentCube.transform.position.y,
-            Mathf.Round(currentCube.transform.position.z));
-
-
-        currentCube.transform.localScale = new Vector3(lastCube.transform.localScale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x),
-                                                        lastCube.transform.localScale.y,
-                                                       lastCube.transform.localScale.z - Mathf.Abs(currentCube.transform.position.z - lastCube.transform.position.z));
-
-        currentCube.transform.position = Vector3.Lerp(currentCube.transform.position, lastCube.transform.position, 0.5f) + Vector3.up * 5f;
-    }
-}
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
+        if(lastCube != null)
+        {
         
+            currentCube.transform.position = new Vector3(Mathf.Round(currentCube.transform.position.x),
+                currentCube.transform.position.y,
+                Mathf.Round(currentCube.transform.position.z));
+
+
+            currentCube.transform.localScale = new Vector3(lastCube.transform.localScale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x),
+                                                            lastCube.transform.localScale.y,
+                                                        lastCube.transform.localScale.z - Mathf.Abs(currentCube.transform.position.z - lastCube.transform.position.z));
+
+            currentCube.transform.position = Vector3.Lerp(currentCube.transform.position, lastCube.transform.position, 0.5f) + Vector3.up * 5f;
+
+            if (currentCube.transform.localScale.x <= 0f ||
+                currentCube.transform.localScale.z <= 0f)
+            {
+             Done = true;
+                text.gameObject.SetActive(true);
+                text.text = "Final Score " + Level;
+                StartCoroutine(X());
+                return;
+            }
+
+
+            
+        }
+
+ lastCube = currentCube;
+        currentCube = Instantiate(lastCube);
+        currentCube.name = Level + "";
+        Level++;
+        Camera.main.transform.position = currentCube.transform.position + new Vector3(100, 100, 100);
+        Camera.main.transform.LookAt(currentCube.transform.position);
+        currentCube.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.HSVToRGB((Level / 100f) % 1f, 1f, 1f));
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+       
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Newblock();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+           if (Done)
+           {
+            return;
+           } 
+           var time = Mathf.Abs(Time.realtimeSinceStartup % 2f - 1f);
+           var pos1 = lastCube.transform.position + Vector3.up * 10f;
+           var pos2 = pos1 + ((Level % 2 == 0) ? Vector3.left : Vector3.forward) * 120;
+           if(Level % 2 == 0)
+           {
+                currentCube.transform.position = Vector3.Lerp(pos2, pos1, time);
+           }
+           else
+           {
+                currentCube.transform.position = Vector3.Lerp(pos1, pos2, time);
+           }
+           if (Input.GetMouseButtonDown(0))
+           {
+               Newblock();
+           }
+        }
+    
+    IEnumerator X()
     {
-        
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("SampleScene");
     }
 }
